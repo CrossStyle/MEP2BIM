992a81a19f78e776abe3bdab09a1695fd171e9c2
Forest <43562257+CrossStyle@users.noreply.github.com>
2025-03-12 12:56:59 +0800

    Add files via upload

---
+++ demo3/pipe_creation.py
@@ -0,0 +1,56 @@
+import math
+import ifcopenshell
+import ifcopenshell.geom
+import ifcopenshell.util
+from OCC.Core.BRepPrimAPI import BRepPrimAPI_MakeSphere
+from OCC.Core.gp import gp_Pnt
+from ifcopenshell.util.placement import get_local_placement
+import numpy as np
+
+
+def create_sphere(cx, cy, cz, radius):
+    sphere_location = gp_Pnt(cx, cy, cz)
+    sphere = BRepPrimAPI_MakeSphere(sphere_location, radius).Shape()
+    return sphere
+
+
+class Patcher:
+    def __init__(self, file, args=None):
+        self.file = file
+        self.args = args
+
+    def patch(self, product, transformation):
+        absolute_placement = self.get_absolute_placement(product.ObjectPlacement)
+        a = transformation @ ifcopenshell.util.placement.get_local_placement(absolute_placement)
+        absolute_placement.RelativePlacement = self.get_relative_placement(a)
+
+    def get_absolute_placement(self, object_placement):
+        if object_placement.PlacementRelTo:
+            return self.get_absolute_placement(object_placement.PlacementRelTo)
+        return object_placement
+
+    def z_rotation_matrix(self, angle):
+        return [
+            [math.cos(angle), -math.sin(angle), 0., 0.],
+            [math.sin(angle), math.cos(angle), 0., 0.],
+            [0., 0., 1., 0.],
+            [0., 0., 0., 1.],
+        ]
+
+    def get_relative_placement(self, m):
+        x = np.array((m[0][0], m[1][0], m[2][0]))
+        z = np.array((m[0][2], m[1][2], m[2][2]))
+        o = np.array((m[0][3], m[1][3], m[2][3]))
+        object_matrix = ifcopenshell.util.placement.a2p(o, z, x)
+        return self.create_ifc_axis_2_placement_3d(
+            object_matrix[:, 3][0:3],
+            object_matrix[:, 2][0:3],
+            object_matrix[:, 0][0:3],
+        )
+
+    def create_ifc_axis_2_placement_3d(self, point, up, forward):
+        return self.file.createIfcAxis2Placement3D(
+            self.file.createIfcCartesianPoint(point.tolist()),
+            self.file.createIfcDirection(up.tolist()),
+            self.file.createIfcDirection(forward.tolist()),
+        )
